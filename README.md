f533c0fd0199ad3cc0023528c7560c2fefd44d31
Chaitanya <chaitanya.aggarwal.07@gmail.com>
2023-01-11 21:48:04 +0530

    added pipelines for docx, png, jpeg, jpg and pdfs

--- README.md
+++ README.md
@@ -2,5 +2,16 @@ This repository contains my attempt for I_Hack by E-Cell IIT Bombay.
 I'm planning to use CV along with classical image processing for the task
 
 Dependencies
-PIL ( should be installed automatically )
+PIL ( pip install Pillow )
 pdf2image ( pip install pdf2image )
+docxtopdf ( pip install docxtopdf )
+subprocess ( builtin for versions above python2.7 )
+
+Input Files
+Make sure that the name without the extension is unique. ( person.docx and person.jpg should not be present together )
+Input formats accepted:
+docx ( the older .doc version was dropped from MS Word 2007, it is highly unlikely someone is still using doc files )
+pdf
+jpeg
+jpg
+png
---
+++ tojpg.py
@@ -0,0 +1,71 @@
+#Script to convert different types of files into jpgs
+
+import PIL as pil
+import os
+import pdf2image
+import subprocess
+
+INPUT_PATH = "InputData/"
+OUTPUT_PATH = "ImageData/"
+
+def pdftoJpg(inputName):
+    # load the pdf file
+    images = pdf2image.convert_from_path(INPUT_PATH+inputName)
+    
+    # loop through the pages and convert them to jpg images
+    for i in range(len(images)):
+        images[i].save(OUTPUT_PATH+inputName[:-4]+"_page"+str(i)+'.jpg', 'JPEG')
+
+def docxtoJpg(inputName):
+
+    #convert the docx to a pdf usiing abiword
+    bashCommand = "abiword --to=pdf "+INPUT_PATH+inputName
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    
+    #convert the pdf to jpg
+    pdftoJpg(inputName[:-4]+"pdf")
+    
+    #delete the newly created pdf
+    bashCommand = "rm "+INPUT_PATH+inputName[:-4]+"pdf"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+
+def pngtoJpg(inputName): 
+    #open image in png format
+    img_png = pil.Image.open(INPUT_PATH+inputName)
+    
+    #The image object is used to save the image in jpg format
+    img_png.save(OUTPUT_PATH+inputName[:-4]+"_page"+ str(0) +'.jpg', 'JPEG')
+
+def jpegtoJpg(inputName):
+    #open image in jpeg format
+    img_jpeg = pil.Image.open(INPUT_PATH+inputName)
+
+    #The image object is used to save the image in jpg format
+    img_jpeg.save(OUTPUT_PATH+inputName[:-5]+"_page"+ str(0) +'.jpg', 'JPEG')
+
+def jpgtoJpg(inputName):
+    #open image in jpeg format
+    img_jpeg = pil.Image.open(INPUT_PATH+inputName)
+
+    #The image object is used to save the image in jpg format
+    img_jpeg.save(OUTPUT_PATH+inputName[:-4]+"_page"+ str(0) +'.jpg', 'JPEG')
+
+if __name__=="__main__":
+    #get the list of all files in input
+    inputData = os.listdir(INPUT_PATH)
+    print(str(inputData))
+
+    #convert each file to jpg format
+    for i in inputData:
+        extension = i.rpartition(".")[2]
+        if extension=="png":
+            pngtoJpg(i)
+        elif extension=="pdf":
+            pdftoJpg(i)
+        elif extension=="docx":
+            docxtoJpg(i)
+        elif extension=="jpeg":
+            jpegtoJpg(i)
+        elif extension=="jpg":
+            jpgtoJpg(i)
